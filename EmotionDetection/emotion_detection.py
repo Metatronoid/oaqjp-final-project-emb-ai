9d8644b61d03a289c436297e6786622a66f2a663
Metatronoid <srivasamrit@gmail.com>
2026-09-16 03:03:21 -0400

    Initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    my_obj = { "raw_document": { "text": text_to_analyze } }
+
+    response = requests.post(url, json = my_obj, headers = header, timeout = 10)
+
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+
+    emotion_scores = {"anger": anger_score, "disgust": disgust_score, "fear": fear_score, "joy": joy_score, "sadness": sadness_score}
+    dominant_emotion = max(emotion_scores, key = emotion_scores.get)
+    emotion_scores["dominant_emotion"] = dominant_emotion
+
+    return emotion_scores
